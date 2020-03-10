62542b8ab9ac491223e4f0fd060ea5c7c784e902
Ilya <ymh9w@yandex.ru>
2020-03-11 00:39:22 +0300

    Solution v1

---
+++ dz4.go
@@ -0,0 +1,29 @@
+package math
+
+import "fmt"
+// Calc is a function that return an operation function
+func Calc(operation CalcOperation) func(a, b float64) float64 {
+	// Place your solution here
+	switch operation { 
+	case "+":
+		return func(a, b float64) float64{
+			return (a+b);
+		}
+	case "-":
+		return func(a, b float64) float64{
+			return (a-b);
+		}
+	case "*":
+		return func(a, b float64) float64{
+			return (a*b);
+		}
+	case "/":
+		return func(a, b float64) float64{
+			return (a/b);
+		}
+	}
+	return func(a, b float64) float64{
+		fmt.Println("unsuported operation, 0.0 means nothing")
+		return 0.0
+	};
+}
---
+++ dz4_test.go
@@ -0,0 +1,68 @@
+package math
+
+import (    
+	"testing" 
+)
+
+// CalcOperation is a type that define allowed operations for calculator
+type CalcOperation string;
+
+// Predefined operations
+const (
+	Add      = "+"
+	Subtract = "-"
+	Multiply = "*"
+	Divide   = "/"
+)
+
+type CalcTest struct{
+	a float64;
+	b float64;
+	op CalcOperation;
+	exp float64;	
+}
+
+
+// returns 4 test cases, [0] = "+", [1] = "-", [2] = "*", [3] = "/"
+func CalcTestsFabric(a,b float64) [4]CalcTest {
+	return [4]CalcTest {
+		{a: a, b: b, op: Add, exp: a+b},
+		{a: a, b: b, op: Subtract, exp: a-b},
+		{a: a, b: b, op: Multiply, exp: a*b},
+		{a: a, b: b, op: Divide, exp: a/b},
+	}
+}
+
+var tests = [][4]CalcTest {
+	CalcTestsFabric(1,2),
+	CalcTestsFabric(1,-2),
+	CalcTestsFabric(-1,-2),
+	CalcTestsFabric(-1,2),
+
+	CalcTestsFabric(100,200),
+	CalcTestsFabric(100,-200),
+	CalcTestsFabric(-100,-200),
+	CalcTestsFabric(-100,200),
+
+	CalcTestsFabric(0,5),
+	CalcTestsFabric(5,0),
+	CalcTestsFabric(-5,0),
+	CalcTestsFabric(5,-0),
+
+	//вот тут тесты поломаются. Работа с нулями подвела :)
+	CalcTestsFabric(0,0),
+	CalcTestsFabric(-0,-0),
+}
+
+func TestCalcAdd(t *testing.T) {
+    for _, curTests := range tests {
+    	for _, curTest := range curTests{
+    		t.Log(curTest)
+    		if Calc(curTest.op)(curTest.a, curTest.b) != curTest.exp {
+    		t.Log("breaks on", curTest)
+    		t.Errorf("%f%s%f should be %f", curTest.a, curTest.op, curTest.b, curTest.exp)
+    		}
+    	}
+    	
+    }
+}
